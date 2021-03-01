e6b7a32c3fdbd06ca7ffff08430af3b999981e3d
gsanchez <gonzalo.sanchezsilva@gmail.com>
2021-03-01 19:16:19 +0000

    feat: exercise 1.5 completed

--- part1/src/index.js
+++ part1/src/index.js
@@ -10,10 +10,10 @@ const Part = (props) => {
 const Header = (props) => {
   return (
     <h1>{props.course}</h1>
-  )
-}
-
-const Content = (props) => {
+    )
+  }
+  
+  const Content = (props) => {
   return (
   <div>
     <Part part={props.parts[0]} />
@@ -24,7 +24,6 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
-  console.log(props)
   let total = 0;
   for (let i in props.parts) {
     total += props.parts[i].exercises
@@ -35,27 +34,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course}/>
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name}/>
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
