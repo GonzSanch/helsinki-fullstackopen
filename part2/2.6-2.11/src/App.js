909483318839044aa6712dcbffc359dd56d7ee56
gsanchez <gonzalo.sanchezsilva@gmail.com>
2021-03-29 23:39:36 +0200

    feat: 2.11 completed phonebook

--- part2/2.6-2.11/src/App.js
+++ part2/2.6-2.11/src/App.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import Axios from 'axios'
+import axios from 'axios'
 
 const Filter = ({ value, handler }) => (
   <div> filter shown with <input value={value} onChange={handler} /></div>
@@ -25,16 +27,19 @@ const PersonsList = ({ toShow }) => (
 )
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [ persons, setPersons ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filter, setfilter ] = useState('')
   
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
   const AddNewPerson = (event) => {
     event.preventDefault()
     if (persons.some(person => person.name === newName)) {
