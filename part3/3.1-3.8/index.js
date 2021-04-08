4f9eb5af4b3e494e80c488d94c88e5d1f21ab595
gsanchez <gonzalo.sanchezsilva@gmail.com>
2021-04-08 16:50:47 +0000

    feat: 3.7 and 3.78 completed middleware implementation

--- part3/3.1-3.8/index.js
+++ part3/3.1-3.8/index.js
@@ -1,32 +1,45 @@
 const { request, response } = require('express')
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const PORT = 3001
 
 let persons = [
-      {
+    {
         "name": "Arto Hellas",
         "number": "040-123456",
         "id": 1
-      },
-      {
+    },
+    {
         "name": "Ada Lovelace",
         "number": "39-44-5323523",
         "id": 2
-      },
-      {
+    },
+    {
         "name": "Dan Abramov",
         "number": "12-43-234345",
         "id": 3
-      },
-      {
+    },
+    {
         "name": "Mary Poppendieck",
         "number": "39-23-6423122",
         "id": 4
-      }
-    ]
+    }
+]
+
+const logger = morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      JSON.stringify(req.body)
+    ].join(' ')
+  })
 
 app.use(express.json())
+app.use(logger)
 
 const generateId = () => {
     const maxId = persons.length > 0
