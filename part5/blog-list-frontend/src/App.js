e4fdf04676304ccae4971ce6b0dae3c93333b3b3
gsanchez <gonzalo.sanchezsilva@gmail.com>
2021-04-17 23:58:46 +0200

    feat: frontlogin feature 5.1

--- part5/blog-list-frontend/src/App.js
+++ part5/blog-list-frontend/src/App.js
@@ -1,15 +1,69 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Notification from './components/Notification'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [message, setMessage] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
-    )  
+    ) 
   }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setMessage('Wrong credentials')
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
+    }
+  }
+  
+  if (user === null) {
+    return (
+      <div>
+        <h1>log in to application</h1>
+        <Notification message={message} />
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+              <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({ target }) => setUsername(target.value)}
+              />
+          </div>
+          <div>
+            password
+              <input
+              type="password"
+              value={password}
+              name="Password"
+              onChange={({ target }) => setPassword(target.value)}
+              />
+          </div>
+          <button type='submit'>login</button>
+        </form>
+        </div>
+    )
+  }
 
   return (
     <div>
