f245a2260bdfe305ed51cb8efbf3f57f181ab912
gsanchez <gonzalo.sanchezsilva@gmail.com>
2021-04-21 00:22:27 +0200

    feat: completed 5.16

--- part5/blog-list-frontend/src/components/CreateBlogForm.test.js
+++ part5/blog-list-frontend/src/components/CreateBlogForm.test.js
@@ -8,7 +8,30 @@ describe('<CreateBlogFrom />', () => {
     test('updates states and calls onSubmit with proper props', () => {
         const createBlog = jest.fn()
 
-        cont component =
+        const component = render(
+            <CreateBlogForm createBlog={createBlog} message={null}/>
+        )
+
+        const input_title = component.container.querySelector('form div:nth-child(1) input')
+        const input_author = component.container.querySelector('form div:nth-child(2) input')
+        const input_url = component.container.querySelector('form div:nth-child(3) input')
+        const form = component.container.querySelector('form')
+
+        fireEvent.change(input_title, {
+            target: { value: 'test form create' }
+        })
+        fireEvent.change(input_author, {
+            target: { value: 'Jest' }
+        })
+        fireEvent.change(input_url, {
+            target: { value: 'localtest' }
+        })
+        fireEvent.submit(form)
+
+        expect(createBlog.mock.calls).toHaveLength(1)
+        expect(createBlog.mock.calls[0][0].title).toBe('test form create')
+        expect(createBlog.mock.calls[0][0].author).toBe('Jest')
+        expect(createBlog.mock.calls[0][0].url).toBe('localtest')
     })
 
 })
