284e70235edf16f455b17c7bcc049906b43a2440
gsanchez <gonzalo.sanchezsilva@gmail.com>
2021-05-11 13:26:56 +0200

    feat: complete webpack lesson

--- part7/webpack-manual/src/App.js
+++ part7/webpack-manual/src/App.js
@@ -1,5 +1,40 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import PromisePolyfill from 'promise-polyfill'
+
+if (!window.Promise) {
+    window.Promise = PromisePolyfill
+}
+
+const useNotes = (url) => {
+    const [notes, setNotes] = useState([])
+
+    useEffect(() => {
+        axios.get(url).then(response => {
+            setNotes(response.data)
+        })
+    }, [url])
+    return notes
+}
+
 const App = () => {
-    return null
+    const [counter, setCounter] = useState(0)
+    const [values, setValues] = useState([])
+    
+    const url = BACKEND_URL
+    const notes = useNotes(BACKEND_URL)
+
+    const handleClick = () => {
+        setCounter(counter + 1)
+        setValues(values.concat(counter))
+    }
+    return (
+        <div className="container">
+            hello webpack {counter} clicks
+            <button onClick={handleClick}>press</button>
+            <div>{notes.length} notes on server {url}</div>
+        </div>
+    )
 }
 
 export default App
