02f2fd185e86f8b17784ae09ecdd6a460404d911
gsanchez <gonzalo.sanchezsilva@gmail.com>
2021-05-17 16:27:22 +0200

    feat: completed apollo client exercises

---
+++ part8/library-graphql/library-frontend/src/components/Authors.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+import Select from 'react-select'
+
+import { useQuery, useMutation } from '@apollo/client'
+import { ALL_AUTHORS, EDIT_AUTHOR } from '../queries'
+
+const Authors = (props) => {
+  const [name, setName] = useState(null)
+  const [born, setBorn] = useState('')
+
+  const authors = useQuery(ALL_AUTHORS)
+  const [updateAuthor] = useMutation(EDIT_AUTHOR)
+
+  if (!props.show) {
+    return null
+  }
+
+  if (authors.loading) {
+    return <div>loading...</div>
+  }
+
+  const submit = async (event) => {
+    event.preventDefault()
+
+    updateAuthor({ variables: { name: name.value, born: Number(born) } })
+
+    setName('')
+    setBorn('')
+  }
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              born
+            </th>
+            <th>
+              books
+            </th>
+          </tr>
+          {authors.data.allAuthors.map(a =>
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+      <h2>set birthyear</h2>
+      <form onSubmit={submit}>
+        <Select
+          defaultValue={name}
+          onChange={setName}
+          options={authors.data.allAuthors.map(a => ({value: a.name, label: a.name}))}
+        />
+        <div>
+          born
+          <input
+            value={born}
+            onChange={({ target }) => setBorn(target.value)}
+          />
+        </div>
+        <button type='submit'>update author</button>
+      </form>
+    </div>
+  )
+}
+
+export default Authors
